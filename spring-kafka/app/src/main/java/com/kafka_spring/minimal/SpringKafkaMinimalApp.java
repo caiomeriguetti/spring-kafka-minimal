d21f84bbb3f251dbabfa78d4e2b649c03536ddb1
Caio Meriguetti <caiomeriguetti@gmail.com>
2021-06-29 22:55:37 +0200

    minimal spring kafka app

---
+++ spring-kafka/app/src/main/java/com/kafka_spring/minimal/SpringKafkaMinimalApp.java
@@ -0,0 +1,13 @@
+package com.kafka_spring.minimal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMinimalApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaMinimalApp.class, args);
+	}
+
+}
---
+++ spring-kafka/app/src/main/java/com/kafka_spring/minimal/resources/KafkaResource.java
@@ -0,0 +1,25 @@
+package com.kafka_spring.minimal.resources;
+
+import com.kafka_spring.minimal.services.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaResource {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaResource(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
